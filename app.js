4d29ef38d191e6c44723bc45e932edec9225ee28
Anton <rgeargerge@yandex.ru>
2021-05-13 00:38:02 +0300

    add cors options

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const cors = require('cors'); 
-const path = require('path');
 const config = require('./config');
 const parsePosts = require('./parser/parsePosts');
 
 const port = process.env.PORT || 3000;
 const app = express();
 
-app.get('/api/posts', cors(), async (req, res) => {
+const whitelist = ['https://collect-media.netlify.app/'];
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+}
+
+app.get('/api/posts', cors(corsOptions), async (req, res) => {
     const posts = [];
     for (const key in config) {
         const data = await parsePosts(config[key].url, config[key]);
